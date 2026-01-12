1342aa2236bee92abecbb8ebcc929fb22b3ebf14
mfikriam <mfikriam.pro@gmail.com>
2026-01-12 21:52:15 +0800

    466. Advanced: Server Components in Client Components

--- app/_components/SelectCountry.js
+++ app/_components/SelectCountry.js
@@ -4,8 +4,7 @@ import { getCountries } from '@/app/_lib/data-service';
 
 async function SelectCountry({ defaultCountry, name, id, className }) {
   const countries = await getCountries();
-  const flag =
-    countries.find((country) => country.name === defaultCountry)?.flag ?? '';
+  const flag = countries.find((country) => country.name === defaultCountry)?.flag ?? '';
 
   return (
     <select
@@ -15,7 +14,7 @@ async function SelectCountry({ defaultCountry, name, id, className }) {
       defaultValue={`${defaultCountry}%${flag}`}
       className={className}
     >
-      <option value=''>Select country...</option>
+      <option value="">Select country...</option>
       {countries.map((c) => (
         <option key={c.name} value={`${c.name}%${c.flag}`}>
           {c.name}
