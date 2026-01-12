17517f1415c3365cf531ca66fd5acd3583ff2e40
mfikriam <mfikriam.pro@gmail.com>
2026-01-12 21:41:37 +0800

    465. Sharing State Between Client and Server: The URL

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,6 +1,7 @@
 import { Suspense } from 'react';
 import CabinList from '@/app/_components/CabinList';
 import Spinner from '@/app/_components/Spinner';
+import Filter from '@/app/_components/Filter';
 
 export const revalidate = 3600; // in seconds
 // export const revalidate = 15;
@@ -9,7 +10,9 @@ export const metadata = {
   title: 'Cabins',
 };
 
-export default function Page() {
+export default function Page({ searchParams }) {
+  const filter = searchParams?.capacity ?? 'all';
+
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">Our Luxury Cabins</h1>
@@ -21,8 +24,12 @@ export default function Page() {
         peaceful, calm vacation. Welcome to paradise.
       </p>
 
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
+
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
