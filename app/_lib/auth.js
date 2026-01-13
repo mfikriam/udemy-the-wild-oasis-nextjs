79901b79f5d6aba391730b7ae7f1d0be6f345770
mfikriam <mfikriam.pro@gmail.com>
2026-01-13 16:20:02 +0800

    477. Creating a New Guest on First Sign In

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,3 +1,4 @@
+import { createGuest, getGuest } from '@/app/_lib/data-service';
 import NextAuth from 'next-auth';
 import Google from 'next-auth/providers/google';
 
@@ -11,6 +12,26 @@ const authConfig = {
   callbacks: {
     authorized({ auth, request }) {
       return !!auth?.user;
+    },
+    async signIn({ user, account, profile }) {
+      try {
+        const existingGuest = await getGuest(user.email);
+
+        if (!existingGuest)
+          await createGuest({
+            email: user.email,
+            fullName: user.name,
+          });
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+    async session({ session, user }) {
+      const guest = await getGuest(session.user.email);
+      session.user.guestId = guest.id;
+      return session;
     },
   },
   pages: {
